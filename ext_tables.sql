c55491fa2c070c3e8a26705626af8e629d066183
kj187 <kj187@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-05-13 13:41:01 +0000

    Initial import to forge.typo3.org
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kj_imagelightbox2/trunk@9597 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_tables.sql
@@ -0,0 +1,8 @@
+#
+# Table structure for table 'tt_content'
+#
+CREATE TABLE tt_content (
+	tx_kjimagelightbox2_imagelightbox2 tinyint(3) DEFAULT '0' NOT NULL
+	tx_kjimagelightbox2_imageset tinyint(3) DEFAULT '0' NOT NULL
+	tx_kjimagelightbox2_presentationmode tinyint(3) DEFAULT '0' NOT NULL
+);
