4aade3d1a130f0de900c0497ab558efbb668596f
kj187 <kj187@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-10-07 10:49:07 +0000

    delete all files - this release is moved to branches/1.4.4
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kj_imagelightbox2/trunk@12755 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++
@@ -1,8 +0,0 @@
-#
-# Table structure for table 'tt_content'
-#
-CREATE TABLE tt_content (
-	tx_kjimagelightbox2_imagelightbox2 tinyint(3) DEFAULT '0' NOT NULL
-	tx_kjimagelightbox2_imageset tinyint(3) DEFAULT '0' NOT NULL
-	tx_kjimagelightbox2_presentationmode tinyint(3) DEFAULT '0' NOT NULL
-);
