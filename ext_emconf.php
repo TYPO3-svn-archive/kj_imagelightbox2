a8def59de000addbcc9625bd80cfce767f22d3f0
kj187 <kj187@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-01-07 20:48:21 +0000

    Security bugfix. Please update!
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kj_imagelightbox2/trunk@28519 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "kj_imagelightbox2"
+# Extension Manager/Repository config file for ext "kj_imagelightbox2".
 #
-# Auto generated 07-10-2008 12:41
+# Auto generated 07-01-2010 20:46
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '2.0.0',
+	'version' => '2.0.1',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:36:{s:9:"ChangeLog";s:4:"3d84";s:10:"README.txt";s:4:"dbd9";s:12:"ext_icon.gif";s:4:"82ab";s:17:"ext_localconf.php";s:4:"c4b7";s:14:"ext_tables.php";s:4:"ac35";s:14:"ext_tables.sql";s:4:"7b17";s:16:"locallang_db.xml";s:4:"6f83";s:19:"doc/wizard_form.dat";s:4:"7452";s:20:"doc/wizard_form.html";s:4:"387a";s:38:"static/KJ__Imagelightbox/constants.txt";s:4:"03f1";s:34:"static/KJ__Imagelightbox/setup.txt";s:4:"baa7";s:33:"lib/class.tx_kjimagelightbox2.php";s:4:"745a";s:39:"lib/class.tx_kjimagelightbox2_print.php";s:4:"feac";s:25:"res/icons/bullet_disk.png";s:4:"e75d";s:28:"res/icons/bullet_picture.png";s:4:"da42";s:19:"res/icons/cross.png";s:4:"4249";s:20:"res/icons/delete.png";s:4:"6846";s:19:"res/icons/image.png";s:4:"82ab";s:21:"res/icons/printer.png";s:4:"2424";s:27:"res/lightbox2.04/index.html";s:4:"38c0";s:33:"res/lightbox2.04/css/lightbox.css";s:4:"9ff2";s:34:"res/lightbox2.04/images/bullet.gif";s:4:"1f7d";s:33:"res/lightbox2.04/images/close.gif";s:4:"540f";s:38:"res/lightbox2.04/images/closelabel.gif";s:4:"0e54";s:41:"res/lightbox2.04/images/donate-button.gif";s:4:"fa03";s:41:"res/lightbox2.04/images/download-icon.gif";s:4:"6b66";s:35:"res/lightbox2.04/images/image-1.jpg";s:4:"2059";s:35:"res/lightbox2.04/images/loading.gif";s:4:"7e99";s:37:"res/lightbox2.04/images/nextlabel.gif";s:4:"110a";s:37:"res/lightbox2.04/images/prevlabel.gif";s:4:"fb5f";s:35:"res/lightbox2.04/images/thumb-1.jpg";s:4:"9b1b";s:30:"res/lightbox2.04/js/builder.js";s:4:"c688";s:30:"res/lightbox2.04/js/effects.js";s:4:"3b4e";s:31:"res/lightbox2.04/js/lightbox.js";s:4:"6ae8";s:32:"res/lightbox2.04/js/prototype.js";s:4:"95e1";s:36:"res/lightbox2.04/js/scriptaculous.js";s:4:"75d1";}',
+	'_md5_values_when_last_written' => 'a:37:{s:9:"ChangeLog";s:4:"3d84";s:10:"README.txt";s:4:"dbd9";s:12:"ext_icon.gif";s:4:"82ab";s:17:"ext_localconf.php";s:4:"c4b7";s:14:"ext_tables.php";s:4:"ac35";s:14:"ext_tables.sql";s:4:"7b17";s:16:"locallang_db.xml";s:4:"6f83";s:21:"doc/imagelightbox.gif";s:4:"6045";s:19:"doc/wizard_form.dat";s:4:"7452";s:20:"doc/wizard_form.html";s:4:"387a";s:33:"lib/class.tx_kjimagelightbox2.php";s:4:"745a";s:39:"lib/class.tx_kjimagelightbox2_print.php";s:4:"aea1";s:25:"res/icons/bullet_disk.png";s:4:"e75d";s:28:"res/icons/bullet_picture.png";s:4:"da42";s:19:"res/icons/cross.png";s:4:"4249";s:20:"res/icons/delete.png";s:4:"6846";s:19:"res/icons/image.png";s:4:"82ab";s:21:"res/icons/printer.png";s:4:"2424";s:27:"res/lightbox2.04/index.html";s:4:"38c0";s:33:"res/lightbox2.04/css/lightbox.css";s:4:"9ff2";s:34:"res/lightbox2.04/images/bullet.gif";s:4:"1f7d";s:33:"res/lightbox2.04/images/close.gif";s:4:"540f";s:38:"res/lightbox2.04/images/closelabel.gif";s:4:"0e54";s:41:"res/lightbox2.04/images/donate-button.gif";s:4:"fa03";s:41:"res/lightbox2.04/images/download-icon.gif";s:4:"6b66";s:35:"res/lightbox2.04/images/image-1.jpg";s:4:"2059";s:35:"res/lightbox2.04/images/loading.gif";s:4:"7e99";s:37:"res/lightbox2.04/images/nextlabel.gif";s:4:"110a";s:37:"res/lightbox2.04/images/prevlabel.gif";s:4:"fb5f";s:35:"res/lightbox2.04/images/thumb-1.jpg";s:4:"9b1b";s:30:"res/lightbox2.04/js/builder.js";s:4:"c688";s:30:"res/lightbox2.04/js/effects.js";s:4:"3b4e";s:31:"res/lightbox2.04/js/lightbox.js";s:4:"6ae8";s:32:"res/lightbox2.04/js/prototype.js";s:4:"95e1";s:36:"res/lightbox2.04/js/scriptaculous.js";s:4:"75d1";s:38:"static/KJ__Imagelightbox/constants.txt";s:4:"03f1";s:34:"static/KJ__Imagelightbox/setup.txt";s:4:"baa7";}',
 	'suggests' => array(
 	),
 );
--- lib/class.tx_kjimagelightbox2_print.php
+++ lib/class.tx_kjimagelightbox2_print.php
@@ -21,11 +21,29 @@
 *
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
+
 /**
  * Imagelightbox v2
+ * PrintVersion
+ * 
  * @author	Julian Kleinhans <typo3@kj187.de>
  */
- 
+
+define('PATH_thisScript',str_replace('//','/', str_replace('\\','/', (PHP_SAPI=='cgi'||PHP_SAPI=='isapi' ||PHP_SAPI=='cgi-fcgi')&&($_SERVER['ORIG_PATH_TRANSLATED']?$_SERVER['ORIG_PATH_TRANSLATED']:$_SERVER['PATH_TRANSLATED'])? ($_SERVER['ORIG_PATH_TRANSLATED']?$_SERVER['ORIG_PATH_TRANSLATED']:$_SERVER['PATH_TRANSLATED']):($_SERVER['ORIG_SCRIPT_FILENAME']?$_SERVER['ORIG_SCRIPT_FILENAME']:$_SERVER['SCRIPT_FILENAME']))));
+define('PATH_site', dirname(dirname(dirname(dirname(dirname(PATH_thisScript))))).'/');
+define('PATH_typo3conf', PATH_site . 'typo3conf/');
+define('PATH_t3lib', PATH_site . 't3lib/');
+
+require_once(PATH_t3lib . 'class.t3lib_div.php');
+require_once(PATH_typo3conf . 'localconf.php');
+
+$image = t3lib_div::_GET('image');
+
+//first check if the requested file has an valid image file extension, not the nicest security feature but at least it prevents from downloading php files like localconf.php.
+$allowedExtensions = t3lib_div::trimExplode(',', (strlen($TYPO3_CONF_VARS['GFX']['imagefile_ext']) > 0 ? $TYPO3_CONF_VARS['GFX']['imagefile_ext'] : 'gif,jpg,jpeg,tif,bmp,pcx,tga,png,pdf,ai'), 1);
+
+$imageInfo = pathinfo($image);
+if(!in_array(strtolower($imageInfo['extension']), $allowedExtensions)) { die('You are trying to download a file, which you don\'t have access to'); }
 
 echo '
 <html>
@@ -33,7 +51,7 @@ echo '
     	<title>Print Image</title>
 	</head>
 	<body onload="javascript:self.print()">
-		<img src="'.$_GET['image'].'" />
+		<img src="' . $image . '" style="border:none;cursor:pointer;" onclick="self.close()">		
 	</body>
 </html>';
  
